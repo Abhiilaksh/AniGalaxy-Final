a68d8c6811205c81dbe0abb8a35f7926f3f726d5
Abhilaksh <abhilaksh2095@gmail.com>
2024-12-06 20:11:38 +0530

    Added a new col in grid

--- frontend/src/components/RelatedAnime.jsx
+++ frontend/src/components/RelatedAnime.jsx
@@ -4,9 +4,9 @@ export const RelatedAnime = ({ data }) => {
     return (
         <>
        <div className="pt-12 mb-[-35px] pl-4 md:pl-16"> <Heading label="Related Anime"/></div>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-16 sm:pt-24 sm:pl-24 pl-2">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-16 sm:pt-24 sm:pl-12 pl-2">
         
-        {data.slice(0,4).map((anime) => (
+        {data.slice(0,5).map((anime) => (
           <AnimeCard key={anime.id} title={anime.name} image={anime.poster} id={anime.id} />
         ))}
       </div>
--- frontend/src/pages/MostPopular.jsx
+++ frontend/src/pages/MostPopular.jsx
@@ -22,7 +22,7 @@ const MostPopular = () => {
 
     return (
         <>
-         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-16 sm:pt-24 sm:pl-24">
+         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-16 sm:pt-32 sm:pl-12">
           {animeData.map((anime) => (
             <AnimeCard key={anime.id} title={anime.name} image={anime.poster} id={anime.id}/>
           ))}

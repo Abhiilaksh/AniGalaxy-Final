d63aa4ee339ac0b4f78a8371886f71ce765079b4
Abhilaksh <abhilaksh2095@gmail.com>
2024-12-21 03:49:54 +0530

    Changes Uptime Robot

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,12 @@ app.head('/api/v1/Health', (req, res) => {
     // Send a response without a body
     res.status(200).end();
 });
+app.get('/api/v1/check', (req, res) => {
+    // Log the request if needed
+    res.json({
+        msg:"Running"
+    })
+});
 
 
 app.use("/api/v1", mainRouter);

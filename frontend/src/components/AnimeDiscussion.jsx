dc3837c400cbcb71f1ae9058ee5f9e35f0c35247
Abhilaksh <abhilaksh2095@gmail.com>
2024-12-22 18:59:30 +0530

    ASD

--- frontend/src/components/AnimeDiscussion.jsx
+++ frontend/src/components/AnimeDiscussion.jsx
@@ -101,7 +101,7 @@ export const AnimeDiscussion = ({ animeId }) => {
     }
 
     return (
-        <div className="max-w-2xl mx-auto p-4">
+        <div className="max-w-2xl sm:mx-auto mr-8  p-4">
             <Heading label="Discussion" />
             
             {error && (
